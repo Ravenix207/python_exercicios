2100ff5913ed479603cfc889fb4a00755ad370e4
Ravenix207 <mariaclarar2908@gmail.com>
2024-05-07 11:52:22 -0300

    exercicio_4

---
+++ Entrada_Saida/exercicio_4.py
@@ -0,0 +1,6 @@
+prova1 = float(input(" Digite a sua primeira nota: "))
+prova2 = float(input(" Digite sua segunda nota: "))
+prova3 = float( input(" Digite sua terceira nota: "))
+media = (prova1 + prova2 + prova3) /3
+print(f" Sua media é { media}")
+

76fbd34dc9d98a7f8db3ee9573583554e04bfc73
Ravenix207 <mariaclarar2908@gmail.com>
2024-05-07 11:17:44 -0300

    Update exercicio_1.py

--- Entrada_Saida/exercicio_1.py
+++ Entrada_Saida/exercicio_1.py
@@ -1,5 +1,9 @@
 #calcular o resultado
 primeiro = int(input('Digite o primeiro número:'))
-segundo = int(input('Digite o segundo numero'))
+segundo = int(input('Digite o segundo numero:'))
 soma = primeiro + segundo 
-print(soma)
+soma = primeiro * segundo
+print(f"primeiro{ primeiro}\n"
+     f"segundo{ segundo}\n"
+     f" soma{ primeiro + segundo }\n"
+     f" soma{ primeiro * segundo:.2f}\n")

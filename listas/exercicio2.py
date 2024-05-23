17bab5f2f0a6af0b728783634c8f1e4b27c56af6
Ravenix207 <mariaclarar2908@gmail.com>
2024-05-23 12:07:55 -0300

    criacao_exercicio2

---
+++ listas/exercicio2.py
@@ -0,0 +1,9 @@
+numeros = []
+dig = [int(input(' Digita cinco números inteiros: '))]
+for numero in numeros:
+      dig.append(dig+numeros)
+      dig.append(dig>numeros)
+      dig.append(dig<numeros)
+      dig.append(dig*numero/2)
+      print(numeros)
+      print(dig)

184eb718884f17f7fad4d7a95c87260a7fafcd46
Ravenix207 <mariaclarar2908@gmail.com>
2024-05-28 11:17:53 -0300

    atualização_lista\mercado.py

--- listas/mercado_lista.py
+++ listas/mercado_lista.py
@@ -1,2 +1,13 @@
+lista_compras = []
 
+num_itens = int(input('Digite a quantidade de itens: '))
+
+for i in range(num_itens):
+    produto = input('Digite o produto: ')
+    quantidade = int(input('Digite a quantidade: '))
+    lista_compras.append([produto, quantidade])
+
+print('Lista de Compra')
+for produto, quantidade in lista_compras:
+    print(f"{produto} --> {quantidade}")
 

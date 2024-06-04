de124da2d96276add15dcc89181b1f4249dc7e2a
Ravenix207 <mariaclarar2908@gmail.com>
2024-06-04 10:40:56 -0300

    criacao do exercicio_1/funcoes

---
+++ funcoes/exercicio_1.py
@@ -0,0 +1,12 @@
+#criar uma função
+def calcularMedia(n1,n2,n3,n4):
+    media = (n1+n2+n3+n4)/4
+    return media
+
+print(' Calculadora de Média')
+n1 = float(input(' Digite a 1ª nota: '))
+n2 = float(input(' Digite a 2ª nota: '))
+n3 = float(input(' Digite a 3ª nota: '))
+n4 = float(input(' Digite a 4ª nota: '))
+m = calcularMedia(n1,n2,n3,n4)
+print (f' Sua média é {m}')

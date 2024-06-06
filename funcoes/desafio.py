34c72600fdab88b974899a486fc056333d484356
Ravenix207 <mariaclarar2908@gmail.com>
2024-06-06 09:11:32 -0300

    melhora do desafio.py

--- funcoes/desafio.py
+++ funcoes/desafio.py
@@ -7,6 +7,32 @@ def cadastrarNotas(dic_alunos):
         notas.append(nota)
     dic_alunos[aluno] = notas
 
+def calcularMedia(notas):
+    media = sum(notas)/len(notas)
+    return media
+
+def statusFinal(media):
+    if(media >=5 ):
+        return 'Aprovado'
+    else:
+        return 'Reprovado' 
+    
+def mostrarNotas(dic_alunos):
+    print(f"\nNOTAS CADASTRADAS")
+    for nome_aluno, notas_aluno in dic_alunos.items():
+        media = calcularMedia(notas_aluno) 
+        print(f'Nome: {nome_aluno} - Notas: {notas_aluno} - Média: {media} - Status: {statusFinal(media)}')
+    print('\n')
+
+def alunosReprovados(dic_alunos):
+    print(f"\nALUNOS REPROVADOS")
+    for nome_aluno, notas_aluno in dic_alunos.items():
+        media = calcularMedia(notas_aluno)
+        if media < 5:
+            print(f"Nome: {nome_aluno} - Média: {media}")
+    print('\n')
+
+
 # Dicionário que vai receber os alunos
 alunos = {}
 
@@ -14,15 +40,18 @@ alunos = {}
 while True:
     print (f'1. Cadastra alunos e notas \n'
         f'2. Mostrar notas cadastradas \n'
-        f'3. Sair do programa \n')
+        f'3. Alunos Reprovados \n'
+        f'0. Sair do programa \n')
     
     opcao = int(input(' Digite uma das opções: '))
     
     if opcao == 1:
         cadastrarNotas(alunos)
     elif opcao == 2:
-        print('Fazer função para mostrar as notas')
+        mostrarNotas(alunos)
     elif opcao == 3:
+        alunosReprovados(alunos)
+    elif opcao == 0:
         break
     else:
         print('Opção inválida')
